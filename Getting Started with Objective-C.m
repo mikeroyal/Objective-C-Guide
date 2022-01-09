b0a1bebd3595312ab616f6cf57fa8fcf454b88f8
Michael Royal <45159366+mikeroyal@users.noreply.github.com>
2022-01-09 14:02:52 -0800

    Updated file.

--- Getting Started with Objective-C.m
+++ Getting Started with Objective-C.m
@@ -1,1 +1,14 @@
 Code samples & snippets coming soon!
+
+
+//Sample for saying Hello!
+
+#import <Foundation/Foundation.h>
+ 
+int main (int argc, const char * argv[])
+{
+        NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+        NSLog (@"Hello!");
+        [pool drain];
+        return 0;
+}
